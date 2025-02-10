3d74bf226520003d7c5eaffea7430c5bb7c0712d
Bankyeiei <bank23113@gmail.com>
2025-02-10 16:33:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("My first ML App (Study on Imbalanced Data Classification by 6713070172
 # Input fields
 features = []
 features_mean = [6.50e+00, 2.50e-01, 3.00e-01, 5.00e+00, 5.00e-02, 4.00e+01, 1.40e+02, 9.90e-01, 3.20e+00, 5.00e-01, 1.00e+01]
-features_step = [0.5, 0.05, 0.05, 2.5, 0.01, 10.0, 20.0, 0.002, 0.1, 0.05, 0.5]
+features_step = [0.5, 0.05, 0.05, 0.25, 0.01, 10.0, 20.0, 0.002, 0.1, 0.05, 0.5]
 
 for i in range(11):  # Adjust based on dataset
     if i != 7 :
